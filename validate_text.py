8d21463a5c214bb8cf2b187dc5322b63ebb6338e
ANDERSON WEBER <andweber92@gmail.com>
2023-05-22 13:48:00 -0300

    Add files via upload

--- validate_text.py
+++ validate_text.py
@@ -1,46 +1,31 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, length, lower, when
+from pyspark.sql.types import StructType, StructField, StringType
+from pyspark.sql.functions import when, col, length, trim, lower, isnull
 
-# Configura a sessão do Spark
-spark = SparkSession.builder.appName("ExampleApp").getOrCreate()
+# Create SparkSession
+spark = SparkSession.builder \
+    .appName("example_app") \
+    .getOrCreate()
 
-# Criação do dataframe de exemplo
-data = [("John", "95.789%"), ("Alice", "42.5%"), ("Bob", None), ("Bob", "abc")]
-df = spark.createDataFrame(data, ["Name", "Percentage"])
+# Sample data
+data = [("John", "Hello"), ("Jane", ""), ("Alice", None), ("Bob", "None"), ("Eve", "  ")]
 
-# Definição da função validate_text
-def validate_text(value, initial_validation: int = 1) -> int:
-    """Valida se o valor é um texto"""
-    if initial_validation == 0 or value is None:
-        return 0
+# Define schema
+schema = StructType([
+    StructField("Name", StringType(), nullable=False),
+    StructField("Text", StringType(), nullable=True)
+])
 
-    stripped = lower(value)
-    return when((stripped != "none") & (stripped != "null") & (length(value) >= 1), 1).otherwise(0)
+# Create DataFrame
+df = spark.createDataFrame(data, schema)
 
-# Aplica a função validate_text utilizando funções nativas do PySpark
-df = df.withColumn("ValidatedText", validate_text(col("Name")))
+# Validate Text column
+df = df.withColumn(
+    "ValidatedText", 
+    when(
+        ~((trim(lower(col("Text"))) == "none") | (trim(lower(col("Text"))) == "null") | (length(trim(col("Text"))) == 0) | isnull(col("Text"))), 1
+    ).otherwise(0)
+)
+
+# Display modified DataFrame
 df.show()
-
-def test_validate_text_should_return_1_for_non_empty_string():
-    value = "Hello"
-    assert validate_text(value) == 1
-
-def test_validate_text_should_return_0_for_empty_string():
-    value = ""
-    assert validate_text(value) == 0
-
-def test_validate_text_should_return_0_for_none():
-    value = None
-    assert validate_text(value) == 0
-
-def test_validate_text_should_return_0_for_none_string():
-    value = "None"
-    assert validate_text(value) == 0
-
-def test_validate_text_should_return_0_for_null_string():
-    value = "Null"
-    assert validate_text(value) == 0
-
-def test_validate_text_should_return_0_for_whitespace_string():
-    value = "   "
-    assert validate_text(value) == 0
